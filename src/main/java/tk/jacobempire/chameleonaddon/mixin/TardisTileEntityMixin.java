148c679c6e05320d3ccd60497bce158b31a2872e
Torchwood-1 <jacobroberts@virginmedia.com>
2023-11-13 23:23:12 +0000

    created TardisTileEntityMixin

---
+++ src/main/java/tk/jacobempire/chameleonaddon/mixin/TardisTileEntityMixin.java
@@ -0,0 +1,9 @@
+package tk.jacobempire.chameleonaddon.mixin;
+
+import com.swdteam.common.tileentity.TardisTileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TardisTileEntity.class)
+public class TardisTileEntityMixin extends TardisTileEntity {
+
+}
