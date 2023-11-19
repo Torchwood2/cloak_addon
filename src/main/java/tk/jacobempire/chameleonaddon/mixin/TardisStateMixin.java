59f2b90ba1180c91630fcfe3b995deafdf3558bc
Torchwood-1 <jacobroberts@virginmedia.com>
2023-11-19 21:01:55 +0000

    tried to make TardisStateMixin, didn't work

---
+++ src/main/java/tk/jacobempire/chameleonaddon/mixin/TardisStateMixin.java
@@ -0,0 +1,8 @@
+package tk.jacobempire.chameleonaddon.mixin;
+
+import com.swdteam.common.tardis.TardisState;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TardisState.class)
+public class TardisStateMixin extends TardisState{
+}
