f907a5f31bf9305b35461b3ef7c9d07f90c939b2
Torchwood-1 <jacobroberts@virginmedia.com>
2023-11-21 21:07:45 +0000

    added git ignore, remove tardis state mixin

--- src/main/java/tk/jacobempire/chameleonaddon/mixin/TardisStateMixin.java
+++
@@ -1,8 +0,0 @@
-package tk.jacobempire.chameleonaddon.mixin;
-
-import com.swdteam.common.tardis.TardisState;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(TardisState.class)
-public class TardisStateMixin extends TardisState{
-}
