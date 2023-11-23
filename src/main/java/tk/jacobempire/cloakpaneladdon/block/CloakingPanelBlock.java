525088e86d12d0f42037d4e7e8dcf68a1a9536f0
Torchwood-1 <jacobroberts@virginmedia.com>
2023-11-23 00:37:15 +0000

    finished

--- src/main/java/tk/jacobempire/cloakpaneladdon/block/CloakingPanelBlock.java
+++ src/main/java/tk/jacobempire/cloakpaneladdon/block/CloakingPanelBlock.java
@@ -7,13 +7,19 @@ import com.swdteam.common.tardis.Location;
 import com.swdteam.common.tardis.TardisData;
 import com.swdteam.common.tileentity.TardisTileEntity;
 import com.swdteam.util.ChatUtil;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import tk.jacobempire.cloakpaneladdon.data.ITardisTileEntityMixin;
@@ -21,6 +27,21 @@ import tk.jacobempire.cloakpaneladdon.data.ITardisTileEntityMixin;
 public class CloakingPanelBlock extends AbstractRotateableWaterLoggableBlock {
     public CloakingPanelBlock(Properties properties) {
         super(properties);
+    }
+    public static final VoxelShape PANEL_SHAPE_BASE = Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0) ;
+    protected static final VoxelShape TEST = VoxelShapes.or(PANEL_SHAPE_BASE, new VoxelShape[0]);;
+
+
+    public BlockRenderType getRenderShape(BlockState p_149645_1_) {
+        return BlockRenderType.MODEL;
+    }
+
+    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return TEST;
+    }
+
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return TEST;
     }
 
     @Override
@@ -40,4 +61,11 @@ public class CloakingPanelBlock extends AbstractRotateableWaterLoggableBlock {
         }
         return super.use(blockState, world, blockPos, playerEntity, hand, blockRayTraceResult);
     }
+
+    public BlockState updateShape(BlockState state1, Direction dir, BlockState state2, IWorld world, BlockPos pos1, BlockPos pos2) {
+        return dir == Direction.DOWN && !this.canSurvive(state2, world, pos1) ? Blocks.AIR.defaultBlockState() : super.updateShape(state1, dir, state2, world, pos1, pos2);
+    }
+
+
+
 }
