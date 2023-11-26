26ad650aaf00aea1ae28e0031914650592df0213
Torchwood-1 <jacobroberts@virginmedia.com>
2023-11-26 20:09:10 +0000

    can only be used in tardis dim

--- src/main/java/tk/jacobempire/cloakpaneladdon/block/CloakingPanelBlock.java
+++ src/main/java/tk/jacobempire/cloakpaneladdon/block/CloakingPanelBlock.java
@@ -1,13 +1,17 @@
 package tk.jacobempire.cloakpaneladdon.block;
 
 import com.swdteam.common.block.AbstractRotateableWaterLoggableBlock;
+import com.swdteam.common.init.DMDimensions;
 import com.swdteam.common.init.DMFlightMode;
 import com.swdteam.common.init.DMTardis;
 import com.swdteam.common.tardis.Location;
 import com.swdteam.common.tardis.TardisData;
 import com.swdteam.common.tileentity.TardisTileEntity;
 import com.swdteam.util.ChatUtil;
-import net.minecraft.block.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
@@ -28,8 +32,10 @@ public class CloakingPanelBlock extends AbstractRotateableWaterLoggableBlock {
     public CloakingPanelBlock(Properties properties) {
         super(properties);
     }
-    public static final VoxelShape PANEL_SHAPE_BASE = Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0) ;
-    protected static final VoxelShape TEST = VoxelShapes.or(PANEL_SHAPE_BASE, new VoxelShape[0]);;
+
+    public static final VoxelShape PANEL_SHAPE_BASE = Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0);
+    protected static final VoxelShape TEST = VoxelShapes.or(PANEL_SHAPE_BASE, new VoxelShape[0]);
+    ;
 
 
     public BlockRenderType getRenderShape(BlockState p_149645_1_) {
@@ -46,26 +52,28 @@ public class CloakingPanelBlock extends AbstractRotateableWaterLoggableBlock {
 
     @Override
     public ActionResultType use(BlockState blockState, World world, BlockPos blockPos, PlayerEntity playerEntity, Hand hand, BlockRayTraceResult blockRayTraceResult) {
-        if (world.isClientSide || hand.equals(Hand.OFF_HAND)) return super.use(blockState, world, blockPos, playerEntity, hand, blockRayTraceResult);
-
-        TardisData data = DMTardis.getTardisFromInteriorPos(blockPos);
-        if (data != null && !data.isInFlight() && data.getCurrentLocation() != null && !DMFlightMode.isInFlight(data.getGlobalID())) {
-            Location location = data.getCurrentLocation();
-            ServerWorld serverWorld = world.getServer().getLevel(data.getCurrentLocation().dimensionWorldKey());
-            TileEntity tile = serverWorld.getBlockEntity(location.getPosition().toBlockPos());
-            if (tile instanceof TardisTileEntity) {
-                ITardisTileEntityMixin iTardisTileEntityMixin = (ITardisTileEntityMixin) tile;
-                iTardisTileEntityMixin.setInvisible(!iTardisTileEntityMixin.isInvisible());
-                ChatUtil.sendCompletedMsg(playerEntity, String.format("Invisible : %b", iTardisTileEntityMixin.isInvisible()), ChatUtil.MessageType.STATUS_BAR);
+        if (world.isClientSide || hand.equals(Hand.OFF_HAND))
+            return super.use(blockState, world, blockPos, playerEntity, hand, blockRayTraceResult);
+        if (world.dimension().equals(DMDimensions.TARDIS)) {
+            TardisData data = DMTardis.getTardisFromInteriorPos(blockPos);
+            if (data != null && !data.isInFlight() && data.getCurrentLocation() != null && !DMFlightMode.isInFlight(data.getGlobalID())) {
+                Location location = data.getCurrentLocation();
+                ServerWorld serverWorld = world.getServer().getLevel(data.getCurrentLocation().dimensionWorldKey());
+                TileEntity tile = serverWorld.getBlockEntity(location.getPosition().toBlockPos());
+                if (tile instanceof TardisTileEntity) {
+                    ITardisTileEntityMixin iTardisTileEntityMixin = (ITardisTileEntityMixin) tile;
+                    iTardisTileEntityMixin.setInvisible(!iTardisTileEntityMixin.isInvisible());
+                    ChatUtil.sendCompletedMsg(playerEntity, String.format("Invisible : %b", iTardisTileEntityMixin.isInvisible()), ChatUtil.MessageType.STATUS_BAR);
+                }
             }
         }
         return super.use(blockState, world, blockPos, playerEntity, hand, blockRayTraceResult);
     }
 
+
     public BlockState updateShape(BlockState state1, Direction dir, BlockState state2, IWorld world, BlockPos pos1, BlockPos pos2) {
         return dir == Direction.DOWN && !this.canSurvive(state2, world, pos1) ? Blocks.AIR.defaultBlockState() : super.updateShape(state1, dir, state2, world, pos1, pos2);
     }
-
 
 
 }
